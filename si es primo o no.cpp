f604643e65d151c399104d037b4a680a2ca3d661
Milagros12345 <ruth.mamani.apucusi@ucsp.edu.pe>
2018-08-28 15:26:34 -0500

    los numeros primos

---
+++ si es primo o no.cpp
@@ -0,0 +1,13 @@
+//Que muestre los números del 1 al 100.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"  BIENVENIDOS "<<endl;
+	for(int i=0;i<=100;i++)
+	{
+		cout<<" son las siguiente   "<<i<<endl;
+	}
+	return 0;
+}
